64d4c2c4897bef4f6194f76370be9675063a7ba6
huangliusong <1138127608@qq.comgit config --global user.email 1138127608@qq.com>
2017-08-04 22:15:43 +0800

    商品实体完成

---
+++ src/main/java/com/huangliusong/repository/ProductInfoRepository.java
@@ -0,0 +1,12 @@
+package com.huangliusong.repository;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by huangliusong on 2017/8/4.
+ */
+public class ProductInfoRepository {
+
+}
